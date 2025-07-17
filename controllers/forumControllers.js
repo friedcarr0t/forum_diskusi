352ef0eac2bfaca828451705063dc0986b24fd26
friedcarr0t <musalimrd@gmail.com>
2025-07-17 18:41:29 +0700

    adding routes get diskusi by id

--- controllers/forumControllers.js
+++ controllers/forumControllers.js
@@ -26,7 +26,26 @@ const postDiskusi = (req, res) => {
     )
 }
 
+const getDiskusiId = (req, res) => {
+    const {id} = req.params
+    
+    db.query(`select*from diskusi where id = ?`,
+        [id], (err, result) => {
+            if(err){
+                console.error('Database error', err);
+                return res.status(500).json({error: "masalah database atau koneksi"})
+            }
+            if(result.length === 0){
+                return res.status(404).json({error: "data tidak ditemukan"});
+            } else {
+                return res.status(200).json(result[0]);
+            }
+        }
+    )
+}
+
 module.exports = {
     getDiskusi,
-    postDiskusi
+    postDiskusi,
+    getDiskusiId
 }
--- routes/forumRoutes.js
+++ routes/forumRoutes.js
@@ -5,5 +5,6 @@ const forumControllers = require('../controllers/forumControllers')
 
 router.get('/diskusi', forumControllers.getDiskusi)
 router.post('/diskusi', forumControllers.postDiskusi)
+router.get('/diskusi/:id', forumControllers.getDiskusiId)
 
 module.exports = router
